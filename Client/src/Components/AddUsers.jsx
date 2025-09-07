8bde19c1c41ee97a65968954ad92bb22cbab486f
Gaurav Raner <133702920+GauravRaner@users.noreply.github.com>
2025-09-07 11:50:35 +0530

    Update submit button text in AddUsers component

--- Client/src/Components/AddUsers.jsx
+++ Client/src/Components/AddUsers.jsx
@@ -108,7 +108,7 @@ const AddUsers = () => {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Submit
+              Submit gaurav
             </button>
           </div>
         </form>
